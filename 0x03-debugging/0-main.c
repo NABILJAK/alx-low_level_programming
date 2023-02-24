d586b86008e601358a411713478e9d6c54a707f2
NABILJAK <jnabil877@gmail.com>
2023-02-23 21:19:46 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,21 +3,14 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
-{
-	int i;
-
+{	int i;
 	printf("Infinite loop incoming :(\n");
-
 	 i = 0;
-
 	 /*while (i < 10)*/
 	 /*{*/
 	 /*	putchar(i);*/
 	 /*}*/
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
