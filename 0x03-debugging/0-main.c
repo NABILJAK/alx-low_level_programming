135da4ab3eaa077c3360020e98aa45dadf0a42fb
NABILJAK <jnabil877@gmail.com>
2023-02-23 21:46:10 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
-#include "main.h"
+#include"main.h"
 
 /**
-* main - causes an infinite loop
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-int main(void)
 
-{	
+int main(void)
+{
 	int i;
-	i = 0;
+
+	i = 98 % 2;
 	positive_or_negative(i);
+
 	return (0);
 }
