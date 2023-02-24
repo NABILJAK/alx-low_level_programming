3d5795f70abb9c1e1a0dbeaab3ac91ed9826511c
NABILJAK <jnabil877@gmail.com>
2023-02-23 21:34:43 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 * main - causes an infinite loop
 * Return: 0
 */
 int main(void)
+
 {	int i;
-	printf("Infinite loop incoming :(\n");
-	 i = 0;
-	 /*while (i < 10)*/
-	 /*{*/
-	 /*	putchar(i);*/
-	 /*}*/
-	printf("Infinite loop avoided! \\o/\n");
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
 #include <stdio.h>
@@ -8,4 +9,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif
+#endif /* MAIN_H */
